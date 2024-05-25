d95fe25866a2abb2033a3532b6f0aa23835c0408
Joao Brito <joao.brito@ctw.bmwgroup.com>
2024-05-26 00:56:03 +0100

    Server monitor

--- server.erl
+++ server.erl
@@ -1,38 +1,73 @@
 -module(server).
 
+-import(server_monitor,[startMonitor/2]).
 -import(helper,[get_process_alias/1]).
 
--export([start/1, connectToRouter/4]).
- 
-start(Server) ->
-    Pid = spawn(fun() -> loop([]) end),
-    register(Server, Pid).
+-export([start/2, connectToRouter/4, startWithMonitor/2]).
+
+defaultStart(Server, MonitorName, Servers) ->
+    compile:file(helper),
+    compile:file(server_monitor),  
+    Pid = spawn(fun() -> init(Servers, MonitorName) end),
+    register(Server, Pid),
+    Pid.
+
+start(ServerName, MonitorName) ->
+    Pid = defaultStart(ServerName, MonitorName, []),
+    enableMonitoring(Pid, MonitorName).
+
+startWithMonitor(Server, Monitor) ->
+    Pid = defaultStart(Server, get_process_alias(Monitor), []),
+    io:format("SERVER::~p@~p:: Spawning server with monitor ~p.~n", [Server, Pid, get_process_alias(Monitor)]),
+    Server ! {monitor, Monitor},
+    Pid.
+
+enableMonitoring(Server, MonitorName) ->  
+    compile:file(server_monitor),  
+    io:format("SERVER:~p@~p:: Monitor starting: ~p~n", [get_process_alias(Server), Server, MonitorName]),
+    MonitorPid = startMonitor(Server, MonitorName),
+    Server ! {monitor, MonitorPid},
+    MonitorPid.
+
+init(Clients, MonitorName) ->
+    process_flag(trap_exit, true),
+    loop(Clients, MonitorName).
 
 % Connect to the respective router
 connectToRouter(Server, Name, Router, Remote) -> Server ! {connect_to_router, Name, Router, Remote}.
  
-loop(Clients) ->
+loop(Clients, MonitorName) ->
     receive
         % Connect to router - WORKING ✅
         {connect_to_router, Name, Router, Remote} ->
             net_adm:ping(Remote),
             io:format("Server::~p@~p:: Trying to connect to ~p~n", [get_process_alias(self()), self(), Router]),
             {Router, Remote} ! {add_server, Name, self()},
-            loop(Clients);
+            whereis(MonitorName) ! {Router, Remote},
+            loop(Clients, MonitorName);
         % Receive sucess message from the router connection - WORKING ✅
         {connected, Router} ->
             io:format("Router connected: ~p~n", [Router]),
-            loop(Clients);
+            loop(Clients, MonitorName);
         % Connect client to server - WORKING ✅
         {connect, Client} ->
             io:format("Client connected: ~p~n", [Client]),
-            loop([Client | Clients]);
+            loop([Client | Clients], MonitorName);
         % Receive message from client and broadcast to all clients - WORKING ✅
-        {From, Message} ->
+        {broadcast, From, Message} ->
             io:format("Server received message: ~p From: ~p ~n", [Message, From]),
             lists:foreach(fun(Client) -> Client ! {From, {server, Message}} end, lists:delete(From, Clients)),
-            loop(Clients);
+            loop(Clients, MonitorName);
+        % Monitor messages
+        {monitor, Monitor} ->
+            request_to_monitor(Monitor),
+            loop(Clients, MonitorName);
         % Stop the server
         stop ->
             io:format("Server stopping~n")
     end.
+
+request_to_monitor(Monitor) ->
+    io:format("SERVER::~p@~p:: Request to be monitored by ~p~n", [get_process_alias(self()), self(), Monitor]),
+    Monitor ! {monitor, self()},
+    io:format("SERVER::~p@~p:: Monitor started on ~p~n", [get_process_alias(self()), self(), get_process_alias(Monitor)]).
---
+++ server_monitor.erl
@@ -0,0 +1,42 @@
+-module(server_monitor).
+
+-import(server, [startWithMonitor/2]).
+-import(helper,[get_process_alias/1]).
+
+-export([startMonitor/2]).
+
+startMonitor(Server, MonitorName) -> 
+    Pid = spawn(fun() -> init(Server) end),
+    io:format("SERVER MONITOR::~p@~p:: Monitor requested for ~p@~p~n", [MonitorName, Pid, get_process_alias(Server), Server]),
+    register(MonitorName, Pid),
+    Pid.
+
+init(Server) ->
+    compile:file(helper),
+    process_flag(trap_exit, true),
+    loop(Server).
+
+loop(Server) ->
+    receive
+        % Start Router monitoring
+        {monitor, Server} ->
+            io:format("SERVER MONITOR::~p@~p:: Server ~p@~p being monitored.~n", [get_process_alias(self()), self(), get_process_alias(Server), Server]),
+            link(Server),
+            loop(Server, get_process_alias(Server), "")
+    end.
+
+loop(Server, ServerName, FullRouter) ->
+    receive
+        % Restart Router when it goes down
+        {'EXIT', Server, Reason} ->
+            io:format("SERVER MONITOR::~p@~p::EXIT:: Server ~p@~p is down due to: ~p~n", [get_process_alias(self()), self(), ServerName, Server, Reason]),
+            NewServer = start_server(ServerName),
+            io:format("FULL ROUTING ~p~n", [FullRouter]),
+            loop(NewServer);
+        % Add Router
+        {Router, Remote} ->
+            io:format("SERVER MONITOR::~p@~p:: Router added ~p@~p", [get_process_alias(self()), self(), get_process_alias(Router), Router]),
+            loop(Server, ServerName, {Router, Remote})
+    end.
+
+start_server(ServerName) -> startWithMonitor(ServerName, self()).
