6db320357e7517bf0ec9bdfb344d7f5f0fecc92f
Joao Brito <joao.brito@ctw.bmwgroup.com>
2024-05-25 21:32:14 +0100

    Remove uneeded router

--- router.erl
+++ router.erl
@@ -1,6 +1,3 @@
-% Recevied Pid, ServerId and message From Client 
-% Send the message to the server
-
 -module(router).
 
 -import(router_monitor,[start_monitor/2]).
@@ -10,16 +7,16 @@
 
 start(Router, Monitor) ->
     compile:file(helper),  
-    Pid = spawn(fun() -> init(Router, Monitor) end),
+    Pid = spawn(fun() -> init(Monitor) end),
     register(Router, Pid),
     startMonitor(Pid, Monitor).
 
-init(RouterName, MonitorName) ->
+init(MonitorName) ->
     process_flag(trap_exit, true),
-    loop([], RouterName, MonitorName).
+    loop([], MonitorName).
 
 startWithMonitor(Router, Monitor) ->
-    Pid = spawn(fun() -> init(Router, get_process_alias(Monitor)) end),
+    Pid = spawn(fun() -> init(get_process_alias(Monitor)) end),
     register(Router, Pid),
     io:format("ROUTER::~p@~p:: Spawning router with monitor ~p.~n", [Router, Pid, get_process_alias(Monitor)]),
     Router ! {monitor, Monitor},
@@ -32,44 +29,44 @@ startMonitor(Router, MonitorName) ->
     Router ! {monitor, MonitorPid},
     MonitorPid.
 
-loop(Servers, RouterName, MonitorName) ->
+loop(Servers, MonitorName) ->
     receive
         % Return all servers when requested - WORKING ✅
         {From, servers} ->
             From ! {servers, Servers},
-            loop(Servers, RouterName, MonitorName);
+            loop(Servers, MonitorName);
         % Add server to the routing - WORKING ✅
         {ServerName, Server, add_server} ->
-            add_server(Server, ServerName, Server),
-            loop([{ServerName, Server} | Servers], RouterName, MonitorName);
+            add_server(ServerName, Server),
+            loop([{ServerName, Server} | Servers], MonitorName);
         % Connect client to server - WORKING ✅
         {connect, Client, ServerName} ->
             Server = lists:keyfind(ServerName, 1, Servers),
             io:format("Router connecting client ~p to server ~p~n", [Client, ServerName]),
             ServerId = element(2, Server),
             ServerId ! {connect, Client},
-            loop(Servers, RouterName, MonitorName);
+            loop(Servers, MonitorName);
         % Clients sends message to server - WORKING ✅
         {deprecate, ServerName, Client, Message} ->
             Server = lists:keyfind(ServerName, 1, Servers),
             io:format("Client ~p sending message: ~p to ~p~n", [Client, Message, Server]),
             ServerId = element(2, Server),
             ServerId ! {Client, Message},
-            loop(Servers, RouterName, MonitorName);
+            loop(Servers, MonitorName);
         % Monitor messages
         {monitor, Monitor} ->
             request_to_monitor(Monitor),
-            loop(Servers, RouterName, MonitorName);
+            loop(Servers, MonitorName);
         % Restart Router Monitor when it goes down
         {'EXIT', Monitor, Reason} ->
             io:format("ROUTER::~p@~p::EXIT:: Router ~p is down due to: ~p~n", [get_process_alias(self()), self(), Monitor, Reason]),
             NewMonitor = startMonitor(self(), MonitorName),  
-            loop(Servers, RouterName, get_process_alias(NewMonitor))
+            loop(Servers, get_process_alias(NewMonitor))
     end.
 
-add_server(RouterName, Server, ServerName) ->
+add_server(Server, ServerName) ->
     Server ! {connected, self()},
-    io:format("ROUTER::~p:: Adding server: ~p@~p~n", [RouterName, ServerName, Server]).
+    io:format("ROUTER::~p@~p:: Adding server: ~p@~p~n", [get_process_alias(self()), self(), ServerName, Server]).
 
 request_to_monitor(Monitor) ->
     io:format("ROUTER::~p@~p:: Request to be monitored by ~p~n", [get_process_alias(self()), self(), Monitor]),
